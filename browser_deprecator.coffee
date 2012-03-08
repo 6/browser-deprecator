5384b8d25cf00d1f9df4f17ac774cad45783e8a2
Peter Graham <peterghm@gmail.com>
2012-03-08 01:32:26 -0500

    basic working version

---
+++ browser_deprecator.coffee
@@ -0,0 +1,30 @@
+version_compare = (v1, v2) ->
+  v1 = $.map v1.split('.'), (s, i) -> parseInt(s, 10)
+  v2 = $.map v2.split('.'), (s, i) -> parseInt(s, 10)
+  for i in [0..(v1.length - 1)]
+    return -1 if i is v2.length
+    continue if v1[i] is v2[i]
+    return if v1[i] > v2[i] then -1 else 1
+  return if v1.length < v2.length then 1 else 0
+
+browser_info_hash = ->
+  browser =
+    ua: navigator.userAgent.toLowerCase()
+    version: $.browser.version
+  browser.flag = 'mozilla' if $.browser.mozilla
+  browser.flag = 'msie' if $.browser.msie
+  browser.flag = 'opera' if $.browser.opera
+  browser.flag = 'chrome' if not browser.flag? if /chrome/.test browser.ua
+  browser.flag = 'safari' if not browser.flag? and /safari/.test browser.ua
+  if browser.flag in ['chrome', 'safari']
+    v = if browser.flag is 'chrome' then 'chrome' else 'version'
+    version = browser.ua.substring(browser.ua.indexOf("#{v}/") + v.length + 1)
+    browser.version = version.split(" ")[0]
+  browser
+
+$.extend
+  deprecate: (opts, cb) ->
+    browser = browser_info_hash()
+    $.each opts, (flag, min_version) ->
+      return true if browser.flag isnt flag # continue
+      return cb() if version_compare(min_version, browser.version) < 0
---
+++ browser_deprecator.js
@@ -0,0 +1,72 @@
+(function() {
+  var browser_info_hash, version_compare;
+  version_compare = function(v1, v2) {
+    var i, _ref;
+    v1 = $.map(v1.split('.'), function(s, i) {
+      return parseInt(s, 10);
+    });
+    v2 = $.map(v2.split('.'), function(s, i) {
+      return parseInt(s, 10);
+    });
+    for (i = 0, _ref = v1.length - 1; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+      if (i === v2.length) {
+        return -1;
+      }
+      if (v1[i] === v2[i]) {
+        continue;
+      }
+      if (v1[i] > v2[i]) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
+    if (v1.length < v2.length) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+  browser_info_hash = function() {
+    var browser, v, version, _ref;
+    browser = {
+      ua: navigator.userAgent.toLowerCase(),
+      version: $.browser.version
+    };
+    if ($.browser.mozilla) {
+      browser.flag = 'mozilla';
+    }
+    if ($.browser.msie) {
+      browser.flag = 'msie';
+    }
+    if ($.browser.opera) {
+      browser.flag = 'opera';
+    }
+    if (/chrome/.test(browser.ua) ? !(browser.flag != null) : void 0) {
+      browser.flag = 'chrome';
+    }
+    if (!(browser.flag != null) && /safari/.test(browser.ua)) {
+      browser.flag = 'safari';
+    }
+    if ((_ref = browser.flag) === 'chrome' || _ref === 'safari') {
+      v = browser.flag === 'chrome' ? 'chrome' : 'version';
+      version = browser.ua.substring(browser.ua.indexOf("" + v + "/") + v.length + 1);
+      browser.version = version.split(" ")[0];
+    }
+    return browser;
+  };
+  $.extend({
+    deprecate: function(opts, cb) {
+      var browser;
+      browser = browser_info_hash();
+      return $.each(opts, function(flag, min_version) {
+        if (browser.flag !== flag) {
+          return true;
+        }
+        if (version_compare(min_version, browser.version) < 0) {
+          return cb();
+        }
+      });
+    }
+  });
+}).call(this);
